d2c3acfb17daf63fc63ba840a0753f4ecfe47d3d
pjmicolet <p.micolet@gmail.com>
2018-03-04 23:02:22 +0000

    Changed how the status register works, everything is now a bool since it'll be easier to debug. You can also set the register to a uint8 for simplicity sake. Linked it to the P_ register in internal_registers as well.

--- src/processor/processor.h
+++ src/processor/processor.h
@@ -1,6 +1,49 @@
+#include "../utils/utils.h"
 #include "dll_export.h"
 
+
 namespace processor {
+
+    struct status {
+        // Carry status
+        bool C;
+
+        // Zero status
+        bool Z;
+
+        // IRQ Enabled/Disabled
+        bool I;
+
+        // Decimal mode
+        bool D;
+
+        // Index mode
+        bool X;
+
+        // Memory/Accumulator mode
+        bool M;
+
+        // Overflow status
+        bool V;
+
+        // Negative status
+        bool N;
+
+        auto operator =(uint8 data)
+        {
+            C = data.bit(0);
+            Z = data.bit(1);
+            I = data.bit(2);
+            D = data.bit(3);
+            X = data.bit(4);
+            M = data.bit(5);
+            V = data.bit(6);
+            N = data.bit(7);
+        }
+        // Initial status
+        status() { C = 0; Z = 0; I = 1; D = 0; X = 1; M = 1; V = 0; N = 0; }
+    };
+
     struct internal_registers {
         // Accumulator
         char A_;
@@ -21,39 +64,10 @@ namespace processor {
         char PB_;
 
         // Processor Status
-        char P_;
+        status P_;
 
         // Program Counter
         char PC_;
-    };
-
-    struct status {
-        // Carry status
-        char C;
-
-        // Zero status
-        char Z;
-
-        // IRQ Enabled/Disabled
-        char I;
-
-        // Decimal mode
-        char D;
-
-        // Index mode
-        char X;
-
-        // Memory/Accumulator mode
-        char M;
-
-        // Overflow status
-        char V;
-
-        // Negative status
-        char N;
-
-        // Initial status
-        status() { C = 0; Z = 0; I = 1; D = 0; X = 1; M = 1; V = 0; N = 0; }
     };
 
      PROCESSORExport void test();
